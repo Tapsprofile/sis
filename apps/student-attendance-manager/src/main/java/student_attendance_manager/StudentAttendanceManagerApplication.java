d10b8e10d5030006ce6dd4a9e85797980c634c2f
Gaurav Kumar <gkumarpro.ai@gmail.com>
2024-09-27 13:56:17 +0530

    Newly Added

---
+++ apps/student-attendance-manager/src/main/java/student_attendance_manager/StudentAttendanceManagerApplication.java
@@ -0,0 +1,13 @@
+package student_attendance_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAttendanceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAttendanceManagerApplication.class, args);
+	}
+
+}
