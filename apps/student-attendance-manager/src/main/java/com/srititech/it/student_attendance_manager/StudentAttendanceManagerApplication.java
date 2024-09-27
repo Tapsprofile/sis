801195c70df3c09503612266480d3e3f0f6dcab9
Gaurav Kumar <gkumarpro.ai@gmail.com>
2024-09-27 13:08:57 +0530

    newly added

---
+++ apps/student-attendance-manager/src/main/java/com/srititech/it/student_attendance_manager/StudentAttendanceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.srititech.it.student_attendance_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAttendanceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAttendanceManagerApplication.class, args);
+	}
+
+}
---
+++ apps/student-attendance-manager/src/test/java/com/srititech/it/student_attendance_manager/StudentAttendanceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srititech.it.student_attendance_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAttendanceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
